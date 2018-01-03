e021053a8ebffb120104e28987ab6300a3876cb7
Thomas van der Velde <contact@toolbarthomas.net>
2018-01-03 14:48:10 +0100

    Unified select

--- javascripts/unified-select.js
+++ javascripts/unified-select.js
@@ -23,9 +23,11 @@
                     return;
                 }
 
-                defineObjects(unified_select);
+                defineField(unified_select);
+                defineDropdown(unified_select);
+                defineOptions(unified_select);
                 defineGlyph(unified_select);
-                defineAttributes(unified_select);
+                defineSelectAttributes(unified_select);
                 defineSelectValue(unified_select);
             },
         });
@@ -67,12 +69,12 @@
     }
 
     // Append all elements for the naked select element
-    function defineObjects(unified_select) {
+    function defineField(unified_select) {
 
         //Check if select element has any wrapper elements
         var wrapper = unified_select.closest('.unified-select__wrapper');
 
-        if (wrapper.length !== 0) {
+        if (wrapper.length != 0) {
             return;
         }
 
@@ -80,12 +82,148 @@
 
         wrapper = unified_select.closest('.unified-select__wrapper');
 
-        // Append the select field glyph
+        // Prepend the select field glyph
         wrapper.prepend('<div class="unified-select__glyph-wrapper"></div>');
 
         // Set the select field that will contain the selected value
         wrapper.prepend('<div class="unified-select__field-wrapper"><div class="unified-select__field"><div class="unified-select__field-value"></div></div></div>');
+    }
 
+    // Append the dropdown and add the event listeners for it
+    function defineDropdown(unified_select) {
+        if (!getOption('dropdown')) {
+            return;
+        }
+
+        // Disable the tabindex for our select elementso we
+        // can't trigger the select element with our keyboard
+        unified_select.attr('tabindex', '-1');
+
+        var wrapper = unified_select.closest('.unified-select__wrapper');
+
+        // Set the dropdown panel
+        wrapper.append('<div class="unified-select__dropdown"><div class="unified-select__dropdown__panel"></div></div>');
+
+        var field_wrapper = wrapper.find('.unified-select__field-wrapper');
+
+        // Enable support for tabbing the selectbox
+        field_wrapper.attr('tabindex', '0');
+
+        // Trigger click event on select field wrapping element
+        field_wrapper.on({
+            click: function (event) {
+                event.preventDefault();
+
+                toggleDropdown(unified_select);
+            },
+            focus: function() {
+                focusSelect(unified_select);
+            },
+            blur: function() {
+                blurSelect(unified_select);
+            }
+        });
+
+        // Add the dropdown class on the main element to set all necessary styles correctly
+        wrapper.addClass('js__unified-select--dropdown-ready');
+
+        // Remove focus & blur events on the select element since we can't select it anymore in combination with the custom dropdown
+        unified_select.off('focus blur');
+    }
+
+    function openDropdown(unified_select) {
+        var wrapper = unified_select.closest('.unified-select__wrapper');
+
+        if (wrapper.length === 0) {
+            return;
+        }
+
+        wrapper.addClass('js__unified-select--dropdown-active');
+    }
+
+    function closeDropdown(unified_select) {
+        var wrapper = unified_select.closest('.unified-select__wrapper');
+
+        if (wrapper.length === 0) {
+            return;
+        }
+
+        wrapper.removeClass('js__unified-select--dropdown-active');
+    }
+
+    function toggleDropdown(unified_select) {
+        var wrapper = unified_select.closest('.unified-select__wrapper');
+
+        if (wrapper.length === 0) {
+            return;
+        }
+
+        wrapper.toggleClass('js__unified-select--dropdown-active');
+    }
+
+    // Append all know option tags as interactive DOM elements
+    function defineOptions(unified_select) {
+
+        if (!getOption('dropdown')) {
+            return;
+        }
+
+        var wrapper = unified_select.closest('.unified-select__wrapper');
+        var options = unified_select.find('option');
+
+        // Abort if we have no options.
+        if (options.length === 0) {
+            return;
+        }
+
+        var dropdown__panel = wrapper.find('.unified-select__dropdown__panel');
+        var dropdown__options = dropdown__panel.find('.unified-select__dropdown__options');
+
+
+        // Check if the generated options have been defined
+        // or empty the current previously defined options
+        if (dropdown__options.length === 0) {
+            dropdown__panel.append('<div class="unified-select__dropdown__options"></div>');
+
+            // Update dropdown__options variable after appending it to it's parent
+            dropdown__options = dropdown__panel.find('.unified-select__dropdown__options');
+        }
+        else {
+            dropdown__options.empty();
+        }
+
+        // Append each option
+        options.each(function(index) {
+            var option = $(this);
+
+            // Try to define the label from the label attribute
+            var label = option.attr('label');
+            // Fallback to the actual option text
+            if (typeof label === 'undefined') {
+                label = option.text();
+            }
+
+            var classes = defineOptionAttributes(option);
+
+            dropdown__options.append('<div class="unified-select__dropdown__option' + classes + '" data-unified-select-option-index="' + index + '">' + label + '</div>');
+        });
+
+        // Define options
+        var dropdown__option = $('.unified-select__dropdown__option');
+
+        dropdown__option.on({
+            click : function(event) {
+                var $this = $(this);
+                var index = $this.data('unified-select-option-index');
+
+                if(isNaN(parseFloat(index))) {
+                    return;
+                }
+
+                changeSelectedIndex(unified_select, index);
+
+            }
+        });
     }
 
     function defineGlyph(unified_select) {
@@ -102,7 +240,7 @@
     }
 
     // Add classnames if the select element has attributes defined
-    function defineAttributes(unified_select) {
+    function defineSelectAttributes(unified_select) {
         var attributes = [
             'autofocus',
             'disabled',
@@ -110,16 +248,40 @@
             'required'
         ];
 
+        var unified_select__wrapper = unified_select.closest('.unified-select__wrapper');
+
+        if (unified_select__wrapper.length == 0) {
+            return;
+        }
+
         for (var i = 0; i < attributes.length; i++) {
 
             if (!unified_select.prop(attributes[i])) {
                 continue;
             }
 
-            var unified_select__wrapper = unified_select.parent('.unified-select__wrapper');
-
             unified_select__wrapper.addClass('js__unified-select--' + attributes[i]);
         }
+    }
+
+    function defineOptionAttributes(option) {
+        var attributes = [
+            'disabled',
+            'selected'
+        ];
+
+        var classname = '';
+
+        for (var i = 0; i < attributes.length; i++) {
+
+            if (!option.prop(attributes[i])) {
+                continue;
+            }
+
+            classname += ' js__unified-select__dropdown__option--' + attributes[i];
+        }
+
+        return classname;
     }
 
     function defineSelectValue(unified_select) {
@@ -181,6 +343,11 @@
         var selected_option = unified_select.find('option').eq(index);
         if(selected_option.prop('disabled')) {
             return;
+        }
+
+        // Close the dropdown after change event if we have no multiple attribute defined
+        if (!unified_select.prop('multiple')) {
+            closeDropdown(unified_select);
         }
 
         // Set the new index
